8a2e21c4d1cf75272db19b012a8db83903a21db5
ASWIN S <113883776+aswinsee796@users.noreply.github.com>
2024-06-29 13:50:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
- build: {
-    rollupOptions: {
-      external: ['react-router-dom']
-    }
-  }
-}
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//  build: {
+//     rollupOptions: {
+//       external: ['react-router-dom']
+//     }
+//   }
+// }
   
-)
+// )
+import reactRefresh from '@vitejs/plugin-react-refresh';
+
+export default {
+  plugins: [reactRefresh()],
+  build: {
+    rollupOptions: {
+      external: ['react-dom/client'],
+    },
+  },
+};
